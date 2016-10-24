4fb5c14224024a18e57c81f260c811d2ed78a7ed
meeraramesh-sjsu <meekar67@gmail.com>
2016-10-23 20:53:15 -0700

    Problem: https://leetcode.com/problems/minimum-window-substring/
    Given two strings s and t, find the minimum window in string s where all the characters in string t exists.

---
+++ C++Project/minWindowSubstring.cpp
@@ -0,0 +1,45 @@
+/*
+@author: Meera Ramesh
+@Problem: https://leetcode.com/problems/minimum-window-substring/
+@email: meekar67@gmail.com
+*/
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int needToFind[256]={0};
+        int hasFound[256]={0};
+        int count = 0;
+        int minWindowBegin = -1;
+        int minWindowEnd=-1;
+        int minWindowLength = INT_MAX;
+        int slen = s.size();
+        int tlen = t.size();
+        for(int i=0;i<tlen;i++)
+        	needToFind[t[i]]++;
+    	int begin = 0;
+    	int end = 0;
+    	for(;end<slen;end++)
+    	{
+    			if(needToFind[s[end]]==0) continue;
+    				hasFound[s[end]]++;
+    			if(hasFound[s[end]]<=needToFind[s[end]])
+    	    	count++;
+        		if(count==tlen)
+    			{
+    				while(needToFind[s[begin]]==0 || hasFound[s[begin]]>needToFind[s[begin]])
+    				{
+    					hasFound[s[begin]]--;
+    					begin++;
+    				}
+    			if(end-begin+1 < minWindowLength)
+    			{
+    				minWindowBegin = begin;
+    				minWindowLength = end-begin+1;
+    				minWindowEnd = end;
+    			}
+    			}
+    	}
+    	if(minWindowLength==INT_MAX) return "";
+    	return s.substr(minWindowBegin,minWindowLength);
+        }
+};
