ab57bc9bed96019fde9fa0db38a0e91dca77adf4
meeraramesh-sjsu <meekar67@gmail.com>
2016-10-26 11:18:54 -0700

    @Problem: https://leetcode.com/problems/counting-bits/
    Counting the number of 1's for a given number.
    O(n) solution, where each number uses the number of 1's found in the number before it.
    For ex: 9 = number of 1's in 8 + number of 1's in 1.
    10 = number of 1's in 8 + number of 1's in 2.
    17 = number of 1's in 16 + number of 1's in 1.

---
+++ C++Project/countingBits.cpp
@@ -0,0 +1,37 @@
+/*
+@Problem : https://leetcode.com/problems/counting-bits/
+@Author: Meera Ramesh
+@email: meekar67@gmail.com
+
+*/
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> result;
+        result.push_back(0); 
+        result.push_back(1); 
+        result.push_back(1); 
+        result.push_back(2);
+        result.push_back(1);
+        result.push_back(2);
+        result.push_back(2);
+        result.push_back(3);
+        if(num<=7)
+        {
+            for(int i=7;i>num;i--)
+            result.pop_back();
+            return result;
+        }
+        for(int i=8;i<=num;i++)
+        {
+            if((i & (i-1))==0) result.push_back(1);
+            else
+            {
+            int index = int(pow(2,int(log2(i))));
+            result.push_back(result[index]+result[i-index]);
+            }
+        }
+        return result;
+        
+    }
+};
